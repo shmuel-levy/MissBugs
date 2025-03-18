43d7553b32e53519b695ea738212ea958cb8abc7
shmuel-levy <shmuellevy16@gmail.com>
2025-03-18 17:33:22 +0200

    add local host

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express' 
+
 const app = express() 
-app.get('/', (req, res) => res.send('Hello there')) 
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.get('/', (req, res) => res.send('Hello Shmuel Levy What Are ')) 
+app.get('/api/samuel', (req, res) => res.send('Hello Carmel')) 
+app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030/'))
+console.log('hey:');
